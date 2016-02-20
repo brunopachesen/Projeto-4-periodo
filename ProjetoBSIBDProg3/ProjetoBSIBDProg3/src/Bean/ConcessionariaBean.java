3ab4bd208e0062ba077dd4d5dae91d9c2a2553a1
brunopachesen <brunopachesen@gmail.com>
2016-02-20 16:26:16 -0200

    Added files via upload

---
+++ ProjetoBSIBDProg3/ProjetoBSIBDProg3/src/Bean/ConcessionariaBean.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bean;
+
+/**
+ *
+ * @author Bruno
+ */
+public class ConcessionariaBean {
+    
+   private String idConcessionaria;
+   private String cnpj;
+   private String endereco;
+   private String telefone;
+   private String nome;
+   private CidadeBean cidade;
+
+    /**
+     * @return the idConcessionaria
+     */
+    public String getIdConcessionaria() {
+        return idConcessionaria;
+    }
+
+    /**
+     * @param idConcessionaria the idConcessionaria to set
+     */
+    public void setIdConcessionaria(String idConcessionaria) {
+        this.idConcessionaria = idConcessionaria;
+    }
+
+    /**
+     * @return the cnpj
+     */
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    /**
+     * @param cnpj the cnpj to set
+     */
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    /**
+     * @return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cidade
+     */
+    public CidadeBean getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade the cidade to set
+     */
+    public void setCidade(CidadeBean cidade) {
+        this.cidade = cidade;
+    }
+}
